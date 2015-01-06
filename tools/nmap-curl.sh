f0c717274248ce6f6dfa0439c6d0bfeb062718cf
tan <wqazsedc@gmail.com>
2015-01-06 10:26:27 +0800

    Update nmap-curl.sh
    -n no dns rdns info

--- tools/nmap-curl.sh
+++ tools/nmap-curl.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-nmap $@ -p 443 | while read line
+nmap $@ -n -p 443 | while read line
 do
 	if [[ $line == 'Nmap scan report for'* ]]; then
-		ip=`echo $line |cut -d\( -f2|cut -d\) -f1`
+		ip=`echo ${line:20}`
 	elif [[ $line == *'tcp open  https' ]]; then
 		curl -v -I `echo 'https://'$ip` 2>&1 | while read curline
 		do
