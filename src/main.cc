2b0be14b9763ec2fde89aae2f56f36a99ed29505
Tan Xiang <wqazsedc@gmail.com>
2015-01-18 11:32:10 +0800

    boost asio

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,9 @@
+#include <cstdlib>
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
-int main(int argc,char *argv[]){
+int main(int argc,char* argv[]){
 	return 0;
 }
