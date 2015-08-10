76a3eea07656ae9d58398c397926d32d43c2931c
Tan Xiang <wqazsedc@gmail.com>
2015-08-10 23:15:33 +0800

    link

--- src/server.cc
+++ src/server.cc
@@ -1,8 +1,18 @@
 #include "server.hh"
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 namespace ssev{
+
+
+void server::io_accept(ev::io &watcher, int revents){
+
+}
+
+void server::signal_cb(ev::sig &signal, int revents){
+
+}
 
 static inline int bind_helper(int& fd,unsigned short port = 0){
 	fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -17,6 +27,10 @@ static inline int bind_helper(int& fd,unsigned short port = 0){
 	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("bind");
 	}
+
+	int qlen = 5;                            // Value to be chosen by application
+	setsockopt(fd, SOL_TCP, TCP_FASTOPEN, &qlen, sizeof(qlen));
+
 	socklen_t len = sizeof(addr);
 	if (getsockname(fd, (struct sockaddr *)&addr, &len) == -1)
 		perror("getsockname");
@@ -40,6 +54,10 @@ server::server(unsigned short port ,std::string ip){
 	}
 	io.set<server, &server::io_accept>(this);
 	io.start(bind_fd, ev::READ);
+}
+
+server::~server(){
+	close(bind_fd);
 }
 
 };
